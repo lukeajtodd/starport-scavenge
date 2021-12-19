13c2186f76905acc6ec56ff0a031175d75a60ec3
Luke Todd <luketodd@zoho.com>
2021-12-19 13:05:12 +0000

    Build after run

--- vue/src/store/generated/lukeajtodd/scavenge/lukeajtodd.scavenge.scavenge/index.js
+++ vue/src/store/generated/lukeajtodd/scavenge/lukeajtodd.scavenge.scavenge/index.js
@@ -213,23 +213,6 @@ export default {
                 throw new SpVuexError('QueryClient:QueryCommitAll', 'API Node Unavailable. Could not perform query: ' + e.message);
             }
         },
-        async sendMsgSubmitScavenge({ rootGetters }, { value, fee = [], memo = '' }) {
-            try {
-                const txClient = await initTxClient(rootGetters);
-                const msg = await txClient.msgSubmitScavenge(value);
-                const result = await txClient.signAndBroadcast([msg], { fee: { amount: fee,
-                        gas: "200000" }, memo });
-                return result;
-            }
-            catch (e) {
-                if (e == MissingWalletError) {
-                    throw new SpVuexError('TxClient:MsgSubmitScavenge:Init', 'Could not initialize signing client. Wallet is required.');
-                }
-                else {
-                    throw new SpVuexError('TxClient:MsgSubmitScavenge:Send', 'Could not broadcast Tx: ' + e.message);
-                }
-            }
-        },
         async sendMsgCommitSolution({ rootGetters }, { value, fee = [], memo = '' }) {
             try {
                 const txClient = await initTxClient(rootGetters);
@@ -264,18 +247,20 @@ export default {
                 }
             }
         },
-        async MsgSubmitScavenge({ rootGetters }, { value }) {
+        async sendMsgSubmitScavenge({ rootGetters }, { value, fee = [], memo = '' }) {
             try {
                 const txClient = await initTxClient(rootGetters);
                 const msg = await txClient.msgSubmitScavenge(value);
-                return msg;
+                const result = await txClient.signAndBroadcast([msg], { fee: { amount: fee,
+                        gas: "200000" }, memo });
+                return result;
             }
             catch (e) {
                 if (e == MissingWalletError) {
                     throw new SpVuexError('TxClient:MsgSubmitScavenge:Init', 'Could not initialize signing client. Wallet is required.');
                 }
                 else {
-                    throw new SpVuexError('TxClient:MsgSubmitScavenge:Create', 'Could not create message: ' + e.message);
+                    throw new SpVuexError('TxClient:MsgSubmitScavenge:Send', 'Could not broadcast Tx: ' + e.message);
                 }
             }
         },
@@ -306,6 +291,21 @@ export default {
                 }
                 else {
                     throw new SpVuexError('TxClient:MsgRevealSolution:Create', 'Could not create message: ' + e.message);
+                }
+            }
+        },
+        async MsgSubmitScavenge({ rootGetters }, { value }) {
+            try {
+                const txClient = await initTxClient(rootGetters);
+                const msg = await txClient.msgSubmitScavenge(value);
+                return msg;
+            }
+            catch (e) {
+                if (e == MissingWalletError) {
+                    throw new SpVuexError('TxClient:MsgSubmitScavenge:Init', 'Could not initialize signing client. Wallet is required.');
+                }
+                else {
+                    throw new SpVuexError('TxClient:MsgSubmitScavenge:Create', 'Could not create message: ' + e.message);
                 }
             }
         },
--- vue/src/store/generated/lukeajtodd/scavenge/lukeajtodd.scavenge.scavenge/index.ts
+++ vue/src/store/generated/lukeajtodd/scavenge/lukeajtodd.scavenge.scavenge/index.ts
@@ -265,21 +265,6 @@ export default {
 		},
 		
 		
-		async sendMsgSubmitScavenge({ rootGetters }, { value, fee = [], memo = '' }) {
-			try {
-				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgSubmitScavenge(value)
-				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
-	gas: "200000" }, memo})
-				return result
-			} catch (e) {
-				if (e == MissingWalletError) {
-					throw new SpVuexError('TxClient:MsgSubmitScavenge:Init', 'Could not initialize signing client. Wallet is required.')
-				}else{
-					throw new SpVuexError('TxClient:MsgSubmitScavenge:Send', 'Could not broadcast Tx: '+ e.message)
-				}
-			}
-		},
 		async sendMsgCommitSolution({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -310,21 +295,22 @@ export default {
 				}
 			}
 		},
-		
-		async MsgSubmitScavenge({ rootGetters }, { value }) {
+		async sendMsgSubmitScavenge({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
 				const msg = await txClient.msgSubmitScavenge(value)
-				return msg
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
 					throw new SpVuexError('TxClient:MsgSubmitScavenge:Init', 'Could not initialize signing client. Wallet is required.')
 				}else{
-					throw new SpVuexError('TxClient:MsgSubmitScavenge:Create', 'Could not create message: ' + e.message)
-					
+					throw new SpVuexError('TxClient:MsgSubmitScavenge:Send', 'Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
+		
 		async MsgCommitSolution({ rootGetters }, { value }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -349,6 +335,20 @@ export default {
 					throw new SpVuexError('TxClient:MsgRevealSolution:Init', 'Could not initialize signing client. Wallet is required.')
 				}else{
 					throw new SpVuexError('TxClient:MsgRevealSolution:Create', 'Could not create message: ' + e.message)
+					
+				}
+			}
+		},
+		async MsgSubmitScavenge({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgSubmitScavenge(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new SpVuexError('TxClient:MsgSubmitScavenge:Init', 'Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new SpVuexError('TxClient:MsgSubmitScavenge:Create', 'Could not create message: ' + e.message)
 					
 				}
 			}
